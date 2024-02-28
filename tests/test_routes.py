483fe8634e00e860e5998c7c074cbd0d271477fc
Gerardo Mathus <gerardo@nextia.mx>
2024-02-28 03:52:13 +0000

    tests for create withlist and check_content_type

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,12 +1,15 @@
 """
 Wishlist API Service Test Suite
 """
+
 import os
 import logging
 from unittest import TestCase
 from wsgi import app
 from service.common import status
 from service.models import db, Wishlist
+
+# cspell: ignore psycopg testdb
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
@@ -18,7 +21,7 @@ DATABASE_URI = os.getenv(
 ######################################################################
 # pylint: disable=too-many-public-methods
 class WishlistService(TestCase):
-    """ REST API Server Tests """
+    """REST API Server Tests"""
 
     @classmethod
     def setUpClass(cls):
@@ -42,7 +45,7 @@ class WishlistService(TestCase):
         db.session.commit()
 
     def tearDown(self):
-        """ This runs after each test """
+        """This runs after each test"""
         db.session.remove()
 
     ######################################################################
@@ -50,8 +53,49 @@ class WishlistService(TestCase):
     ######################################################################
 
     def test_index(self):
-        """ It should call the home page """
+        """It should call the home page"""
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
-    # Todo: Add your test cases here...
+    def test_create_wishlist(self):
+        """It should create a new wishlist"""
+
+        # create a new wishlist
+        resp = self.client.post(
+            "/wishlists",
+            json={
+                "name": "test wishlist",
+                "description": "test description",
+                "username": "testuser",
+                "is_public": False,
+                "created_at": "2024-02-27 00:00:00",
+                "last_updated_at": "2024-02-27 00:00:00",
+            },
+        )
+        wishlist = resp.get_json()
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        self.assertIn("id", wishlist)
+        self.assertEqual(wishlist["name"], "test wishlist")
+        self.assertEqual(wishlist["description"], "test description")
+        self.assertEqual(wishlist["username"], "testuser")
+
+    def test_check_content_type(self):
+        """It should check the content type"""
+        wishlist_dict = {
+            "name": "test wishlist",
+            "description": "test description",
+            "username": "testuser",
+            "is_public": False,
+            "created_at": "2024-02-27 00:00:00",
+            "last_updated_at": "2024-02-27 00:00:00",
+        }
+        resp = self.client.post(
+            "/wishlists",
+            query_string=wishlist_dict,
+        )
+        self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+        resp = self.client.post(
+            "/wishlists",
+            data=wishlist_dict,
+        )
+        self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
