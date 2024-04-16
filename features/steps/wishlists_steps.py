65a5c9691aae10f7f35578fca7584ec89ac3c241
Harshbansal1407 <harshbansal1407@gmail.com>
2024-04-16 16:09:43 +0000

    partial working version for create

---
+++ features/steps/wishlists_steps.py
@@ -0,0 +1,55 @@
+######################################################################
+# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+
+"""
+Pet Steps
+
+Steps file for Pet.feature
+
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
+import requests
+from behave import given
+
+# HTTP Return Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
+
+
+@given("the following wishlists")
+def step_impl(context):
+    """Delete all the Wishlists and load the new ones"""
+
+    # List all the wishlists and delete them one by one
+    rest_endpoint = f"{context.base_url}/wishlists"
+    context.resp = requests.get(rest_endpoint)
+    assert context.resp.status_code == HTTP_200_OK
+
+    for wishlist in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{wishlist['id']}")
+        assert context.resp.status_code == HTTP_204_NO_CONTENT
+
+    for row in context.table:
+        payload = {
+            "username": row["username"],
+            "name": row["name"],
+            "is_public": row["is_public"] in ["True", "true", "1"],
+            "description": row["description"],
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
---
+++ features/wishlists.feature
@@ -0,0 +1,37 @@
+Feature: The Wishlist service back-end
+    As a Store Owner
+    I need a RESTful catalog service
+    So that I can keep track of all my Wishlists
+
+Background:
+    Given the following wishlists
+        | id | username | name          | is_public | description        |
+        | 2  | user2    | Wish List     | False     | Private wishlist   |
+        | 3  | user3    | My Wish List  | False     | Private wishlist   |
+
+Scenario: The server is running
+    When I visit the "Home Page"
+    Then I should see "Wishlists RESTful Service" in the title
+    And I should not see "404 Not Found"
+
+Scenario: Create a Wishlist
+    When I visit the "Home Page"
+    And I set the "Username" to "s1"
+    And I set the "Name" to "w1"
+    And I select "True" in the "Is Public" dropdown
+    And I set the "Description" to "d1"
+    And I press the "Create" button
+    Then I should see the message "Success"
+    When I copy the "ID" field
+    And I press the "Clear" button
+    Then the "ID" field should be empty
+    And the "Username" field should be empty
+    And the "Name" field should be empty
+    And the "description" field should be empty
+    When I paste the "ID" field
+    And I press the "Retrieve" button
+    Then I should see the message "Success"
+    And I should see "s1" in the "Username" field
+    And I should see "w1" in the "Name" field
+    And I should see "True" in the "Is Public" dropdown
+    And I should see "d1" in the "Description" field
