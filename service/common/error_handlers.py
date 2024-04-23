29dc65af927d62bf123acf4cf14e306bdb0d1d9a
Atom-Agenda <117327289+Atom-Agenda@users.noreply.github.com>
2024-04-23 16:28:28 -0400

    resolve import error

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -19,14 +19,14 @@ Module: error_handlers
 # from flask import jsonify
 from service import api
 from flask import current_app as app  # Import Flask application
-from service.models import DataValidationError, DatabaseConnectionError
+from service.models import DataValidationError
 from . import status
 
 
 ######################################################################
 # Error Handlers
 ######################################################################
-@api.errorhandler(DataValidationError)
+@app.errorhandler(DataValidationError)
 def request_validation_error(error):
     """Handles Value Errors from bad data"""
     message = str(error)
@@ -36,15 +36,3 @@ def request_validation_error(error):
         "error": "Bad Request",
         "message": message,
     }, status.HTTP_400_BAD_REQUEST
-
-
-@api.errorhandler(DatabaseConnectionError)
-def database_connection_error(error):
-    """Handles Database Errors from connection attempts"""
-    message = str(error)
-    app.logger.critical(message)
-    return {
-        "status_code": status.HTTP_503_SERVICE_UNAVAILABLE,
-        "error": "Service Unavailable",
-        "message": message,
-    }, status.HTTP_503_SERVICE_UNAVAILABLE
